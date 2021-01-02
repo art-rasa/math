fd490afe0433c34980af497c444077d7fb123f94
art-rasa <arttu.rasanen@protonmail.com>
2021-01-03 00:49:21 +0200

    Added test Added a simple test program, allowing the user to test gcDenom and numDigits for possible errors.

--- test/main.f90
+++ test/main.f90
@@ -1,5 +1,57 @@
-program main
-implicit none
+program math_test
+    use math
+    implicit none
+    
+    integer :: a
+    integer :: b
+    integer :: gcd
+    
+    write(*,'(a)') 'Enter number a: '
+    read(*, '(i10)') a
+    write(*,'(a)') 'Enter number b: '
+    read(*, '(i10)') b
+    
+    gcd = gcDenom(a, b)
+    
+    write(*,'(a)') 'Greatest common denominator:'
+    write(*,*) gcd
+    
+    write(*, '(a,i5)') 'Number of digits in a: ', numDigits(a)
+    write(*, '(a,i5)') 'Number of digits in b: ', numDigits(b)
+    
+end program
 
-print *, "Put some tests in here!"
-end program main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
